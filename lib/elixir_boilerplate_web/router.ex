ac4d210cefd1bf40c863641edfaafb065008e38c
Jann Peter Rocas <aeegvk@gmail.com>
2019-06-12 13:35:58 +0000

    Initial commit

---
+++ lib/elixir_boilerplate_web/router.ex
@@ -0,0 +1,22 @@
+defmodule ElixirBoilerplateWeb.Router do
+  use Phoenix.Router
+
+  pipeline :api do
+    plug(:accepts, ["json"])
+  end
+
+  pipeline :browser do
+    plug(:accepts, ["html", "json"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
+    plug(:put_layout, {ElixirBoilerplateWeb.Layouts.View, :app})
+  end
+
+  scope "/", ElixirBoilerplateWeb do
+    pipe_through(:browser)
+
+    get("/", Home.Controller, :index, as: :home)
+  end
+end
